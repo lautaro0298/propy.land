bb17e5c895739ee8a71559967a60b58bd952b223
lautaro0298 <lautaro.0298@gmail.com>
2023-01-06 17:25:30 -0300

    nuevo

---
+++ API Persistencia/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Persistencia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private ConexionDB con;
+        public LoginController(ConexionDB conexion)
+        {
+            con = conexion;
+        }
+        [HttpPost]
+        [Route("google-login")]
+        public async Task<ActionResult> GoogleLogin([FromHeader] string token)
+        {
+            await con.Authenticate(token);
+
+            return Ok();
+        }
+    }
+}

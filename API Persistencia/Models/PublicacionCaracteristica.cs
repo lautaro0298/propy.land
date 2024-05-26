07819f3907729055fd6d550522a1eda4528b02af
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:23 -0300

    improve

--- API Persistencia/Models/PublicacionCaracteristica.cs
+++ API Persistencia/Models/PublicacionCaracteristica.cs
@@ -1,16 +1,46 @@
-﻿using System;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API_Persistencia.Models
 {
     public class PublicacionCaracteristica
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string PublicacionCaracteristicaId { get; set; }
+
+        [Required]
         public string CaracteristicaId { get; set; }
+        public Caracteristica Caracteristica { get; set; }
+
+        [Required]
         public string PublicacionId { get; set; }
-        public virtual Publicacion Publicacion { get; set; }
-        public virtual Caracteristica Caracteristica { get; set; }
+        public Publicacion Publicacion { get; set; }
+
+        [Required]
+        public bool Required { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        public ICollection<PublicacionCaracteristicaValor> Valores { get; set; }
+    }
+
+    public class PublicacionCaracteristicaValor
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string PublicacionCaracteristicaValorId { get; set; }
+
+        [Required]
+        public string PublicacionCaracteristicaId { get; set; }
+        public PublicacionCaracteristica PublicacionCaracteristica { get; set; }
+
+        [Required]
+        public string Valor { get; set; }
     }
 }

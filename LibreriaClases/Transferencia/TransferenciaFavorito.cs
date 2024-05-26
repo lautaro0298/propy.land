fc6208b4272ae02fa1f0281dd05c72e1d22f4b19
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:16 -0300

    improve

--- LibreriaClases/Transferencia/TransferenciaFavorito.cs
+++ LibreriaClases/Transferencia/TransferenciaFavorito.cs
@@ -1,15 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LibreriaClases.Transferencia
 {
+    /// <summary>
+    /// Represents a favorite transfer.
+    /// </summary>
     public class TransferenciaFavorito
     {
+        /// <summary>
+        /// The unique identifier of the favorite.
+        /// </summary>
         public string favoritoId { get; set; }
+
+        /// <summary>
+        /// Indicates whether the favorite is active or not.
+        /// </summary>
         public bool activo { get; set; }
+
+        /// <summary>
+        /// The unique identifier of the publication.
+        /// </summary>
         public string publicacionId { get; set; }
+
+        /// <summary>
+        /// The unique identifier of the user.
+        /// </summary>
         public string usuarioId { get; set; }
+
+        /// <summary>
+        /// The associated publication. It can be null.
+        /// </summary>
         public virtual TransferenciaPublicacion Publicacion { get; set; }
     }
 }

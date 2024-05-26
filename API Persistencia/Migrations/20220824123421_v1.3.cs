a3a3abc057dc20d5b06db05f91a1324a848afee2
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:01 -0300

    improve

--- API Persistencia/Migrations/20220824123421_v1.3.cs
+++ API Persistencia/Migrations/20220824123421_v1.3.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace API_Persistencia.Migrations
 {
-    public partial class v13 : Migration
+    public partial class v13_Improved : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Drop the foreign key and index on TipoPropiedadCaracteristica.propiedadId1
             migrationBuilder.DropForeignKey(
                 name: "FK_TipoPropiedadCaracteristica_Propiedad_propiedadId1",
                 table: "TipoPropiedadCaracteristica");
@@ -14,6 +15,7 @@ namespace API_Persistencia.Migrations
                 name: "IX_TipoPropiedadCaracteristica_propiedadId1",
                 table: "TipoPropiedadCaracteristica");
 
+            // Change the type of propiedadId1 to be nullable
             migrationBuilder.AlterColumn<string>(
                 name: "propiedadId1",
                 table: "TipoPropiedadCaracteristica",
@@ -21,12 +23,14 @@ namespace API_Persistencia.Migrations
                 oldClrType: typeof(string),
                 oldType: "nvarchar(450)");
 
+            // Add a new column propiedadId2 to TipoPropiedadCaracteristica
             migrationBuilder.AddColumn<string>(
                 name: "propiedadId2",
                 table: "TipoPropiedadCaracteristica",
                 nullable: false,
                 defaultValue: "");
 
+            // Change the type of tipoPropiedadId to be nullable
             migrationBuilder.AlterColumn<string>(
                 name: "tipoPropiedadId",
                 table: "Propiedad",
@@ -35,16 +39,19 @@ namespace API_Persistencia.Migrations
                 oldType: "nvarchar(max)",
                 oldNullable: true);
 
+            // Create a new index on TipoPropiedadCaracteristica.propiedadId2
             migrationBuilder.CreateIndex(
                 name: "IX_TipoPropiedadCaracteristica_propiedadId2",
                 table: "TipoPropiedadCaracteristica",
                 column: "propiedadId2");
 
+            // Create a new index on Propiedad.tipoPropiedadId
             migrationBuilder.CreateIndex(
                 name: "IX_Propiedad_tipoPropiedadId",
                 table: "Propiedad",
                 column: "tipoPropiedadId");
 
+            // Add a new foreign key from Propiedad to TipoPropiedad
             migrationBuilder.AddForeignKey(
                 name: "FK_Propiedad_TipoPropiedad_tipoPropiedadId",
                 table: "Propiedad",
@@ -53,6 +60,7 @@ namespace API_Persistencia.Migrations
                 principalColumn: "tipoPropiedadId",
                 onDelete: ReferentialAction.Restrict);
 
+            // Add a new foreign key from TipoPropiedadCaracteristica to Propiedad
             migrationBuilder.AddForeignKey(
                 name: "FK_TipoPropiedadCaracteristica_Propiedad_propiedadId2",
                 table: "TipoPropiedadCaracteristica",
@@ -64,26 +72,32 @@ namespace API_Persistencia.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Propiedad_TipoPropiedad_tipoPropiedadId",
-                table: "Propiedad");
-
+            // Drop the foreign key from TipoPropiedadCaracteristica to Propiedad
             migrationBuilder.DropForeignKey(
                 name: "FK_TipoPropiedadCaracteristica_Propiedad_propiedadId2",
                 table: "TipoPropiedadCaracteristica");
 
-            migrationBuilder.DropIndex(
-                name: "IX_TipoPropiedadCaracteristica_propiedadId2",
-                table: "TipoPropiedadCaracteristica");
+            // Drop the foreign key from Propiedad to TipoPropiedad
+            migrationBuilder.DropForeignKey(
+                name: "FK_Propiedad_TipoPropiedad_tipoPropiedadId",
+                table: "Propiedad");
 
+            // Drop the index on Propiedad.tipoPropiedadId
             migrationBuilder.DropIndex(
                 name: "IX_Propiedad_tipoPropiedadId",
                 table: "Propiedad");
 
+            // Drop the index on TipoPropiedadCaracteristica.propiedadId2
+            migrationBuilder.DropIndex(
+                name: "IX_TipoPropiedadCaracteristica_propiedadId2",
+                table: "TipoPropiedadCaracteristica");
+
+            // Remove the propiedadId2 column from TipoPropiedadCaracteristica
             migrationBuilder.DropColumn(
                 name: "propiedadId2",
                 table: "TipoPropiedadCaracteristica");
 
+            // Change the type of propiedadId1 to be non-nullable
             migrationBuilder.AlterColumn<string>(
                 name: "propiedadId1",
                 table: "TipoPropiedadCaracteristica",
@@ -92,6 +106,7 @@ namespace API_Persistencia.Migrations
                 oldClrType: typeof(string),
                 oldNullable: true);
 
+            // Change the type of tipoPropiedadId to be nullable
             migrationBuilder.AlterColumn<string>(
                 name: "tipoPropiedadId",
                 table: "Propiedad",
@@ -100,11 +115,13 @@ namespace API_Persistencia.Migrations
                 oldClrType: typeof(string),
                 oldNullable: true);
 
+            // Create a new index on TipoPropiedadCaracteristica.propiedadId1
             migrationBuilder.CreateIndex(
                 name: "IX_TipoPropiedadCaracteristica_propiedadId1",
                 table: "TipoPropiedadCaracteristica",
                 column: "propiedadId1");
 
+            // Add a new foreign key from TipoPropiedadCaracteristica to Propiedad
             migrationBuilder.AddForeignKey(
                 name: "FK_TipoPropiedadCaracteristica_Propiedad_propiedadId1",
                 table: "TipoPropiedadCaracteristica",

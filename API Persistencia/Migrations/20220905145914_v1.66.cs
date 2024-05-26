c72d63687c8b0980146aace936c04d749c03d728
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:04:57 -0300

    improve

--- API Persistencia/Migrations/20220905145914_v1.66.cs
+++ API Persistencia/Migrations/20220905145914_v1.66.cs
@@ -1,55 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace API_Persistencia.Migrations
 {
-    public partial class v166 : Migration
+    public partial class v166_Improved : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_PublicacionCaracteristica_Caracteristica_CaracteristicaId",
-                table: "PublicacionCaracteristica");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_PublicacionCaracteristica_Publicacion_PublicacionId",
-                table: "PublicacionCaracteristica");
-
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_PublicacionCaracteristica",
-                table: "PublicacionCaracteristica");
-
-            migrationBuilder.RenameTable(
-                name: "PublicacionCaracteristica",
-                newName: "PublicacionCaracteristicas");
-
-            migrationBuilder.RenameIndex(
-                name: "IX_PublicacionCaracteristica_CaracteristicaId",
-                table: "PublicacionCaracteristicas",
-                newName: "IX_PublicacionCaracteristicas_CaracteristicaId");
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_PublicacionCaracteristicas",
-                table: "PublicacionCaracteristicas",
-                columns: new[] { "PublicacionId", "CaracteristicaId" });
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_PublicacionCaracteristicas_Caracteristica_CaracteristicaId",
-                table: "PublicacionCaracteristicas",
-                column: "CaracteristicaId",
-                principalTable: "Caracteristica",
-                principalColumn: "caracteristicaId",
-                onDelete: ReferentialAction.Cascade);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_PublicacionCaracteristicas_Publicacion_PublicacionId",
-                table: "PublicacionCaracteristicas",
-                column: "PublicacionId",
-                principalTable: "Publicacion",
-                principalColumn: "publicacionId",
-                onDelete: ReferentialAction.Cascade);
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
                 name: "FK_PublicacionCaracteristicas_Caracteristica_CaracteristicaId",
@@ -88,6 +43,51 @@ namespace API_Persistencia.Migrations
             migrationBuilder.AddForeignKey(
                 name: "FK_PublicacionCaracteristica_Publicacion_PublicacionId",
                 table: "PublicacionCaracteristica",
+                column: "PublicacionId",
+                principalTable: "Publicacion",
+                principalColumn: "publicacionId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PublicacionCaracteristica_Caracteristica_CaracteristicaId",
+                table: "PublicacionCaracteristica");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PublicacionCaracteristica_Publicacion_PublicacionId",
+                table: "PublicacionCaracteristica");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PublicacionCaracteristica",
+                table: "PublicacionCaracteristica");
+
+            migrationBuilder.RenameTable(
+                name: "PublicacionCaracteristica",
+                newName: "PublicacionCaracteristicas");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PublicacionCaracteristica_CaracteristicaId",
+                table: "PublicacionCaracteristicas",
+                newName: "IX_PublicacionCaracteristicas_CaracteristicaId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PublicacionCaracteristicas",
+                table: "PublicacionCaracteristicas",
+                columns: new[] { "PublicacionId", "CaracteristicaId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PublicacionCaracteristicas_Caracteristica_CaracteristicaId",
+                table: "PublicacionCaracteristicas",
+                column: "CaracteristicaId",
+                principalTable: "Caracteristica",
+                principalColumn: "caracteristicaId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PublicacionCaracteristicas_Publicacion_PublicacionId",
+                table: "PublicacionCaracteristicas",
                 column: "PublicacionId",
                 principalTable: "Publicacion",
                 principalColumn: "publicacionId",

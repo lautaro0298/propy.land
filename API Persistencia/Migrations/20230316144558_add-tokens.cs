f2d2b183ecdca9d8da6b8d5b67c3d1b096397e70
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:08 -0300

    improve

--- API Persistencia/Migrations/20230316144558_add-tokens.cs
+++ API Persistencia/Migrations/20230316144558_add-tokens.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace API_Persistencia.Migrations
@@ -7,56 +7,26 @@ namespace API_Persistencia.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "CreatedAt",
+            migrationBuilder.DropColumns(
+                new string[] { "CreatedAt", "ExternalId" },
                 table: "Usuario");
 
-            migrationBuilder.DropColumn(
-                name: "ExternalId",
-                table: "Usuario");
-
-            migrationBuilder.AddColumn<string>(
-                name: "AccessToken",
+            migrationBuilder.AddColumns(
+                new[] { "AccessToken", "ExpirationTime", "RefreshToken" },
                 table: "Usuario",
-                nullable: true);
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "ExpirationTime",
-                table: "Usuario",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "RefreshToken",
-                table: "Usuario",
-                nullable: true);
+                new[] { columnType: "nvarchar(100)", nullable: true, columnType: "datetime2", nullable: true, columnType: "nvarchar(100)", nullable: true });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "AccessToken",
+            migrationBuilder.DropColumns(
+                new string[] { "AccessToken", "ExpirationTime", "RefreshToken" },
                 table: "Usuario");
 
-            migrationBuilder.DropColumn(
-                name: "ExpirationTime",
-                table: "Usuario");
-
-            migrationBuilder.DropColumn(
-                name: "RefreshToken",
-                table: "Usuario");
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "CreatedAt",
+            migrationBuilder.AddColumns(
+                new[] { "CreatedAt", "ExternalId" },
                 table: "Usuario",
-                type: "datetime2",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
-
-            migrationBuilder.AddColumn<string>(
-                name: "ExternalId",
-                table: "Usuario",
-                type: "nvarchar(max)",
-                nullable: true);
+                new[] { columnType: "datetime2", nullable: false, columnType: "nvarchar(max)", nullable: true });
         }
     }
 }

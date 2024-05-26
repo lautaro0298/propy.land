49cd6082323b5070ae87e197e98dc368bfd10fb9
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:47 -0300

    improve

--- WebApp/React/src/Routes/Routes.jsx
+++ WebApp/React/src/Routes/Routes.jsx
@@ -1,30 +1,19 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { useState } from "react"
 import AllHotels from "../Components/hotels/AllHotels";
 import { store } from "../store/Store";
 import { Provider } from "react-redux"
-import{ LoadScript } from "@react-google-maps/api"
 
 export const Routes = () => {
-   
   return (
-    <div>
-          <Switch>
-              <LoadScript
-                  googleMapsApiKey="AIzaSyAXxPwQsLSfF4gC7VtJdl9GIIcyAzVdmhk"
-                  libraries={["places"]}
-              >
-
-              <Provider store={store} >
-              <AllHotels />
-              </Provider>
-              </LoadScript>
-          </Switch>
-       
-              
-
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={AllHotels} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
 

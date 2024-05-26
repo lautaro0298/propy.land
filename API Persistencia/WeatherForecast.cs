90d8ade63603c7c07e7de89ff93958f49f3db9bb
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:04:53 -0300

    improve

--- API Persistencia/WeatherForecast.cs
+++ API Persistencia/WeatherForecast.cs
@@ -1,14 +1,25 @@
 using System;
 
-namespace API_Persistencia
+namespace WeatherForecastAPI
 {
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public int? TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF
+        {
+            get
+            {
+                if (!TemperatureC.HasValue)
+                {
+                    throw new InvalidOperationException("TemperatureC is null");
+                }
+
+                return 32 + (int)(TemperatureC.Value / 0.5556);
+            }
+        }
 
         public string Summary { get; set; }
     }

770abcadeff81c76e7f65cd3a0b02c566a8b8fac
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:27 -0300

    improve

--- LibreriaClases/Transferencia/TransferenciaTipoMoneda.cs
+++ LibreriaClases/Transferencia/TransferenciaTipoMoneda.cs
@@ -1,13 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LibreriaClases.Transferencia
 {
-    public class TransferenciaTipoMoneda
+    /// <summary>
+    /// Represents a type of currency used in a transfer.
+    /// </summary>
+    public class CurrencyTypeTransfer
     {
-        public string tipoMonedaId { get; set; }
-        public string denominacionMoneda { get; set; }
-        public bool activo { get; set; }
+        /// <summary>
+        /// The unique identifier of the currency type.
+        /// </summary>
+        public string CurrencyTypeId { get; set; }
+
+        /// <summary>
+        /// The denomination of the currency.
+        /// </summary>
+        public string CurrencyDenomination { get; set; }
+
+        /// <summary>
+        /// Indicates whether the currency type is active or not.
+        /// </summary>
+        public bool IsActive { get; set; }
     }
 }

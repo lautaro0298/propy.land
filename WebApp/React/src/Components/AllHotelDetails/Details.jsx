7a95197604fb0f7e99cafa0867bd1114ca3a226e
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:06:17 -0300

    improve

--- WebApp/React/src/Components/AllHotelDetails/Details.jsx
+++ WebApp/React/src/Components/AllHotelDetails/Details.jsx
@@ -1,41 +1,41 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import "./Details.css";
 import Overview from './Overview';
 import Photos from './Photos';
 
 const HotelDetails = () => {
-    const [photoData,setPhotoData] = useState(false);
-    const [Over,setOver] = useState(false);
-    
+  const [tab, setTab] = useState('overview');
 
-const filterItem1=(e)=>{
-    setPhotoData(false)
-    setOver(true)
-    console.log(`clicked ${e.target.value}`)
-}
-console.log(Over)
+  const handleTabClick = (e) => {
+    const newTab = e.target.value;
+    setTab(prevTab => (prevTab === newTab ? 'overview' : newTab));
+  }
 
-const filterItem2=(e)=>{
-    setOver(false)
-    setPhotoData(true)
-    console.log(`clicked ${e.target.value}`)
-}
+  let component;
+  switch (tab) {
+    case 'overview':
+      component = <Overview key="overview" />;
+      break;
+    case 'photos':
+      component = <Photos key="photos" />;
+      break;
+    default:
+      component = null;
+  }
 
-    return (
-        <div className="b-hd-parent-container-div">
-            <div className="b-all-headings-div">
-              <span><button id="o1" onClick={filterItem1}>Overview</button></span>
-              <span><button id="o2">Info</button></span>
-              <span><button id="o3" onClick={filterItem2}>Photos</button></span>
-              <span><button id="o4">Review</button></span>
-              <span><button id="o5" >Deals</button></span>    
-            </div>
-            <hr/>
-            <Overview Over={Over}/>
-            <Photos photoData={photoData}/>
-        </div>
-        
-    )
+  return (
+    <div className="b-hd-parent-container-div">
+      <div className="b-all-headings-div">
+        <span><button value="overview" onClick={handleTabClick}>Overview</button></span>
+        <span><button value="info">Info</button></span>
+        <span><button value="photos" onClick={handleTabClick}>Photos</button></span>
+        <span><button value="review">Review</button></span>
+        <span><button value="deals">Deals</button></span>
+      </div>
+      <hr/>
+      {component}
+    </div>
+  )
 }
 
 export default HotelDetails

aeaa116b1b984261acb74fad54e7eff9ac2b4ef7
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:58 -0300

    improve

--- WebApp/DTOJSon/DTOViewPort.cs
+++ WebApp/DTOJSon/DTOViewPort.cs
@@ -1,13 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace WebApp.DTOJSon
+namespace ImprovedWebApp.DTOs
 {
-    public class DTOViewPort
+    public class ViewPortDto
     {
-        public DTONortheast northeast { get; set; }
-        public DTOSouthwest southwest { get; set; }
+        public NortheastDto Northeast { get; set; }
+        public SouthwestDto Southwest { get; set; }
+    }
+
+    public class NortheastDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class SouthwestDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }

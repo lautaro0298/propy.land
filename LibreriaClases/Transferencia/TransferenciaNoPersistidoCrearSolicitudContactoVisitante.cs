0090547694cafc83c6313471cc2eef50005f2bb1
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:31 -0300

    improve

--- LibreriaClases/Transferencia/TransferenciaNoPersistidoCrearSolicitudContactoVisitante.cs
+++ LibreriaClases/Transferencia/TransferenciaNoPersistidoCrearSolicitudContactoVisitante.cs
@@ -1,12 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LibreriaClases.Transferencia
 {
-    public class TransferenciaNoPersistidoCrearSolicitudContactoVisitante
+    /// <summary>
+    /// Represents a non-persisted transfer object for creating a visitor contact request.
+    /// </summary>
+    public class TransferenciaNoPersistidaCrearSolicitudContactoVisitante
     {
-        public TransferenciaPlanUsuario planUsuario { get; set; }
-        public TransferenciaSolicitudContactoVisitante solicitudContactoVisitante { get; set; }
+        /// <summary>
+        /// Gets or sets the transfer object for the user's plan.
+        /// </summary>
+        public TransferenciaPlanUsuario PlanUsuario { get; set; }
+
+        /// <summary>
+        /// Gets or sets the transfer object for the visitor contact request.
+        /// </summary>
+        public TransferenciaSolicitudContactoVisitante SolicitudContactoVisitante { get; set; }
     }
 }

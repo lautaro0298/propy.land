eca2a971d0df69dca2cc368be2cb975c8e2e69c3
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:06:07 -0300

    improve

--- WebApp/Controllers/CotizacionController.cs
+++ WebApp/Controllers/CotizacionController.cs
@@ -1,28 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using WebApp.Experto;
 using LibreriaExperto.Comunicaciones_Externas;
 using LibreriaClases.DTOJSon;
-using System.Threading.Tasks;
 
 namespace WebApp.Controllers
 {
     public class CotizacionController : Controller
     {
-        // GET: Cotizacion
-        public async Task<JsonResult>  CrearCotizacion()
+        public CotizacionController()
         {
-            var obj = await APICotizacionToday.GetCotizacionAsync();
-            
-            return Json(obj , JsonRequestBehavior.AllowGet);
+            // Initialize any necessary components here
         }
 
+        // GET: Cotizacion
+        [HttpGet]
+        public async Task<JsonResult> CrearCotizacion()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://api.cotizaciontoday.com/cotizaciones");
+            var content = await response.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<Cotizacion>(content);
+
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+
+        [Obsolete]
         public void ActualizarCotizacion()
         {
-          //  ExpertoCotizacion.ActualizarCotizacion();
+            // ExpertoCotizacion.ActualizarCotizacion();
         }
     }
 }

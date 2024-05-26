785034ce96fc585be52aeeedf558d574bf4c308e
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:07:02 -0300

    improve

--- WebApp/Scripts/react/react-dom.js
+++ WebApp/Scripts/react/react-dom.js
@@ -9,34 +9,41 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  *
  */
+
 // Based off https://github.com/ForbesLindesay/umd/blob/master/template.js
-;(function(f) {
-  // CommonJS
-  if (typeof exports === "object" && typeof module !== "undefined") {
-    module.exports = f(require('react'));
 
-  // RequireJS
-  } else if (typeof define === "function" && define.amd) {
-    define(['react'], f);
+type GlobalObject = {
+  React?: any;
+};
 
-  // <script>
-  } else {
-    var g;
-    if (typeof window !== "undefined") {
-      g = window;
-    } else if (typeof global !== "undefined") {
-      g = global;
-    } else if (typeof self !== "undefined") {
-      g = self;
-    } else {
-      // works providing we're not in "use strict";
-      // needed for Java 8 Nashorn
-      // see https://github.com/facebook/react/issues/3037
-      g = this;
-    }
-    g.ReactDOM = f(g.React);
-  }
+const globalObject: GlobalObject =
+  typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : typeof self !== "undefined"
+    ? self
+    : {};
 
-})(function(React) {
-  return React.__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-});
+const React = globalObject.React;
+
+if (!React) {
+  throw new Error("React not found");
+}
+
+const ReactDOM = React.__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+
+export default ReactDOM;
+
+// CommonJS
+if (typeof exports === "object" && typeof module !== "undefined") {
+  module.exports = ReactDOM;
+
+// RequireJS
+} else if (typeof define === "function" && define.amd) {
+  define(["react"], () => ReactDOM);
+
+// <script>
+} else {
+  globalObject.ReactDOM = ReactDOM;
+}

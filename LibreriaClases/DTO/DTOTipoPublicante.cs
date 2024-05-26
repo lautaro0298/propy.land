6ab1fe69571b06be744e34c80e69727b0fc7f13c
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:14 -0300

    improve

--- LibreriaClases/DTO/DTOTipoPublicante.cs
+++ LibreriaClases/DTO/DTOTipoPublicante.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LibreriaClases.DTO
 {
+    /// <summary>
+    /// Data Transfer Object (DTO) for TipoPublicante
+    /// </summary>
     public class DTOTipoPublicante
     {
-        public string tipoPublicanteId { get; set; }
-        public string nombreTipoPublicante { get; set; }
+        /// <summary>
+        /// The unique identifier for the tipo publicante
+        /// </summary>
+        public string TipoPublicanteId { get; set; }
+
+        /// <summary>
+        /// The name of the tipo publicante
+        /// </summary>
+        public string NombreTipoPublicante { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the DTOTipoPublicante class
+        /// </summary>
+        public DTOTipoPublicante()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DTOTipoPublicante class
+        /// with the specified tipo publicante id and name
+        /// </summary>
+        /// <param name="tipoPublicanteId">The unique identifier for the tipo publicante</param>
+        /// <param name="nombreTipoPublicante">The name of the tipo publicante</param>
+        public DTOTipoPublicante(string tipoPublicanteId, string nombreTipoPublicante)
+        {
+            TipoPublicanteId = tipoPublicanteId;
+            NombreTipoPublicante = nombreTipoPublicante;
+        }
     }
 }

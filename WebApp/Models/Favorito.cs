2612bbe9b022b5c4c61059054121bc73a69649cb
lautaro0298 <lautaro.0298@gmail.com>
2024-05-26 14:05:43 -0300

    improve

--- WebApp/Models/Favorito.cs
+++ WebApp/Models/Favorito.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebApp.Models
 {
@@ -11,16 +12,37 @@ namespace WebApp.Models
     public class Favorito
     {
         [Key]
+        [Required]
+        [StringLength(100)]
         public string favoritoId { get; set; }
+
+        [Required]
         public bool activo { get; set; }
-   
-        
+
+        [Required]
         public string usuarioId { get; set; }
+
         [ForeignKey("usuarioId")]
-        public virtual ApplicationUser ApplicationUser { get; set; }
-        
+        public virtual ApplicationUser? ApplicationUser { get; set; }
+
+        [Required]
         public Guid publicacionId { get; set; }
+
         [ForeignKey("publicacionId")]
-        public virtual Publicacion Publicacion { get; set; }
+        public virtual Publicacion? Publicacion { get; set; }
+
+        [Index("IX_UsuarioPublicacion", 1, 2), Required]
+        public virtual ApplicationUser? ApplicationUserWithPublicacion
+        {
+            get
+            {
+                return ApplicationUser;
+            }
+            set
+            {
+                ApplicationUser = value;
+                Publicacion = null; // Reset Publicacion when ApplicationUser is set
+            }
+        }
     }
 }
